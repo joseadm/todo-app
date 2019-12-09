d81e08e73f8d404c5988362dc068bc6f99284dfb
Jose Delgado Matamoros <jose@Joses-MacBook-Pro.local>
2019-12-08 20:19:09 -0600

    removing file

--- todo-server/src/todo/dto/create-todo.ts
+++
@@ -1,7 +0,0 @@
-export class CreateTodoDTO {
-    readonly id: number;
-    readonly title: string;
-    readonly description: string;
-    readonly completed: string;
-    readonly todoDate: Date;
-}
